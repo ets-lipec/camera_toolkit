59bc9085a11a84b4bf92616d903e97d039e95609
Stéphane HU <stephanehu@MacBook-Pro-de-Stephane.local>
2020-09-30 14:35:17 -0400

    Add first version: create local server, take pictures by refreshing the link from the webcam. Cannot save images yet.

---
+++ snap_server.py
@@ -0,0 +1,37 @@
+#https://raw.githubusercontent.com/miguelgrinberg/flask-video-streaming/master/base_camera.py
+from importlib import import_module
+import PIL
+import os
+from flask import Flask, render_template, Response
+import cv2
+import getpass
+
+# uncomment below to use Raspberry Pi camera instead
+# from camera_pi import Camera
+
+# comment this out if you're not using USB webcam
+from camera_opencv import Camera
+
+app = Flask(__name__)
+
+path= '/Users/stephanehu/Repositories/camera_experiments/image.jpg'
+@app.route('/')
+def index():
+    return("API is up and running, images are being served at /image.jpg")
+
+def gen2(camera):
+    """Returns a single image frame"""
+    frame = camera.get_frame()
+    yield frame
+
+@app.route('/image.jpg')
+def image():
+    """Returns a single current image for the webcam"""
+    imfile=Response( gen2( Camera() ) , mimetype='image/jpeg' )
+    imgfile.save("image1.jpg")
+    
+    #cv2.imwrite(os.path.join(path , 'image1.jpg'), imfile)
+    return imfile
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
